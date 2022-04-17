bba0054f90b4e97b0b3c6b55d609887141791600
mohadeseh <m.ghafoori78@gmail.com>
2022-04-17 23:06:34 +0430

    draw a circle on blank image

--- blank_image.py
+++ blank_image.py
@@ -8,6 +8,9 @@ cv.imshow("blank",blank)
 blank[50:100,150:300]=0,0,255
 cv.imshow("painted image",blank)
 #draw a rectangular 
-cv.rectangle(blank,(0,0),(50,50),(200,155,200),thickness=cv.FILLED)
+cv.rectangle(blank,(0,0),(50,50),(200,155,200),thickness=cv.FILLED) #thickness=-1 is also for filling,but other numbers like 2 will thicken rectangular 
 cv.imshow("image with rectangular",blank)
+#draw a circle
+cv.circle(blank,(250,250),20,(100,182,250),thickness=2)
+cv.imshow("image with circle",blank)
 cv.waitKey(0)
