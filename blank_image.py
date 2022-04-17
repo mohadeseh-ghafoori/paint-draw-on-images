36a3ac5c14057c14e94f37f56038e97d1dfdcaf7
mohadeseh <m.ghafoori78@gmail.com>
2022-04-17 23:02:43 +0430

    draw a rectangular on blank image

--- blank_image.py
+++ blank_image.py
@@ -1,3 +1,4 @@
+from calendar import c
 from pickletools import uint8
 import cv2 as cv
 import numpy as np 
@@ -6,4 +7,7 @@ cv.imshow("blank",blank)
 #paint the image with a certain color
 blank[50:100,150:300]=0,0,255
 cv.imshow("painted image",blank)
+#draw a rectangular 
+cv.rectangle(blank,(0,0),(50,50),(200,155,200),thickness=cv.FILLED)
+cv.imshow("image with rectangular",blank)
 cv.waitKey(0)
